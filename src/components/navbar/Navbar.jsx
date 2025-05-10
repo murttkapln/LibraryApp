187e9e66286f11d167ac2a3f13e740a6eae93c06
murttkapln <murttkapln@gmail.com>
2025-05-10 19:39:17 +0300

    feat: Navbar modified& Navbar Styles created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,40 @@
-
+import { useState } from "react";
+import { menuIcon } from "../../helper/iconData";
+import { HamburgerIcon, Logo, Menu, MenuLink, Nav } from "./Navbar.style";
+import { useAuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
+  const { user, setUser } = useAuthContext();
+  const [toggle, setToggle] = useState(false);
+  const logOut = () => {
+    setUser(false);
+    handleClose()
+  };
+  const handleClose = ()=>{
+    setToggle(false)
+  }
   return (
-    <div>Navbar</div>
-  )
-}
+    <Nav justify="space-between" wrap="wrap">
+      <Logo to="/">Mkap Libraray</Logo>
+      <HamburgerIcon onClick={() => setToggle(!toggle)}>
+        {menuIcon}
+      </HamburgerIcon>
+      <Menu showMenu={toggle}>
+        <MenuLink onClick={handleClose} to="/">Home</MenuLink>
+        <MenuLink onClick={handleClose} to="/about">About</MenuLink>
+        {user ? (
+          <MenuLink to="login" onClick={logOut}>
+            Logout
+          </MenuLink>
+        ) : (
+          <>
+            <MenuLink onClick={handleClose} to="/login">Login</MenuLink>
+            <MenuLink onClick={handleClose} to="/register">Register</MenuLink>
+          </>
+        )}
+      </Menu>
+    </Nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
