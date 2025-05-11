ab526317c8586f26a8147dbfccf98d05f6a68bbb
murttkapln <murttkapln@gmail.com>
2025-05-11 23:16:59 +0300

    feat:header SearchInfo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { useBooksContext } from "../../context/BooksContext";
 import {
   HeaderButton,
   HeaderContainer,
@@ -8,13 +9,42 @@ import {
 } from "./Header.style";
 
 const Header = () => {
+  const printType = ["all", "books", "magazines"];
+  const { searchInfo, setSearchInfo, getData } = useBooksContext();
+  const handleChange = (e) => {
+    console.log(e.target);
+    console.log(e.target.name);
+    console.log({ [e.target.name]: e.target.value });
+    setSearchInfo({ ...searchInfo, [e.target.name]: e.target.value }); // inputun name attr ile statede ki key isimleri aynı olmak zorunda.
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  };
   return (
     <HeaderContainer>
       <HeaderTitle>Books Or Magazines</HeaderTitle>
-      <HeaderForm>
-        <SearchInput type="search" placeholder="search..." />
-        <SelectBox>
-          <option>All</option>
+      <HeaderForm onSubmit={handleSubmit}>
+        <SearchInput
+          type="search"
+          placeholder="search..."
+          name="query"
+          value={searchInfo.query}
+          onChange={handleChange}
+          required
+        />
+        <SelectBox
+          name="selectType"
+          value={searchInfo.selectType}
+          onChange={handleChange}
+          // onChange={()=> setSearchInfo({...searchInfo, query:e.target.value})}
+        >
+          {printType.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
         </SelectBox>
         <HeaderButton>Search</HeaderButton>
       </HeaderForm>
--- src/context/BooksContext.js
+++ src/context/BooksContext.js
@@ -1,28 +1,35 @@
 // Books Context
 
 import axios from "axios";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const BooksContext = createContext();
 
 const BooksContextProvider = ({ children }) => {
-  const [query, setQuery] = useState("");
-  const [selectType, setSelectType] = useState("all");
+  //   const [query, setQuery] = useState("");
+  //   const [selectType, setSelectType] = useState("all");
+  const [searchInfo, setSearchInfo] = useState({
+    query: "",
+    selectType: "all",
+  });
   const [myData, setMyData] = useState([]);
 
-
-  const APP_KEY = process.env.REACT_APP_apiKey
+  const APP_KEY = process.env.REACT_APP_apiKey;
   console.log(APP_KEY);
-  const getData = async ()=>{
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&printType=${selectType}&key=${APP_KEY}`
-    try {
-        const {data} = await axios(url)
-    } catch (error) {
-            console.log(err);
-    }
-  }
 
-  const values = { myData, setMyData };
+  const getData = async () => {
+    // const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&printType=${selectType}&key=${APP_KEY}`;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${searchInfo.query}&printType=${searchInfo.selectType}&key=${APP_KEY}`;
+    try {
+      const { data } = await axios(url);
+      console.log(data);
+      setMyData(data.items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const values = { myData, setMyData, getData, searchInfo, setSearchInfo };
   return (
     <BooksContext.Provider value={{ values }}>{children}</BooksContext.Provider>
   );
