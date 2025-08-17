2bfa3591a9fbacffba1f40141fe3196cebbe98a6
murttkapln <murttkapln@gmail.com>
2025-08-17 19:41:20 +0300

    about  page updated

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -15,12 +15,12 @@ const About = () => {
     <AboutContainer>
       <ProfileImg src={profile} alt="anthony" />
       <InfoContainer>
-        <h2>Hi, I'm Anthony</h2>
+        <h2>Hi, I'm Murat</h2>
         <h3>
           I'm currently learning different full-stack development languages.
         </h3>
         <h4>
-          I already know JavaScript, ReactJS, Next.js, React Native, Django,
+          I already know JavaScript, ReactJS, Next.js, 
           Node.js, MongoDB, SQL, Python, and AWS services.
         </h4>
         <h4>💬 You can ask me anything you want to know</h4>
